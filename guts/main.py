1ba35dee04753925fc2a752917d66d4fd6917a0b
rejohnson547 <re.johnson547@gmail.com>
2019-12-16 18:04:04 -0500

    finished basic scaper

---
+++ guts/main.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+source = requests.get('https://www.imdb.com/title/tt0944947/').text
+
+soup = BeautifulSoup(source, 'html5lib')
+
+artical = soup.find('artical')
+
+title = soup.find('div', class_='title_wrapper')
+summary = soup.find('div', class_='summary_text')
+
+print(title.h1.text)
+print(summary.text.strip())

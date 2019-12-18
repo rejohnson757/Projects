985e42c3e3adecab4b20b8f2b508ad2a0b82a387
rejohnson547 <re.johnson547@gmail.com>
2019-12-18 12:55:37 -0500

    more info

--- guts/main.py
+++ guts/main.py
@@ -9,7 +9,13 @@ artical = soup.find('artical')
 
 title = soup.find('div', class_='title_wrapper')
 summary = soup.find('div', class_='summary_text')
+rating = soup.find('div', class_='ratingValue')
+show_rating = soup.find('div', class_='subtext')
+image = soup.find('div', class_='poster')
 
 print(title.h1.text)
 print('\n')
 print(summary.text.strip())
+print(rating.span.text)
+print(show_rating.time.text.strip())
+print(image.img)

a32f32619e1fc93ea87365fe9c21079dcbc82913
EJ <re.johnson547@gmail.com>
2020-03-18 23:13:10 -0400

    test

--- ROT13.py
+++ ROT13.py
@@ -4,14 +4,17 @@
 '''This program will use Caesar cipher ROT13'''
 
 
-def rot_13(messge):
+def rot_13():
 	text = input('Please enter a message: ')
+	lst = []
 	for letter in text:
 		letter = ord(letter)
 		letter = letter + 13
 		if letter > ord('z'):
 			(letter - ord('z')) + ord('a')
-	print(letter)
+		lst.append(chr(letter))
+		
+	print(lst)
 	
-rot_13('hello')
+rot_13()
 

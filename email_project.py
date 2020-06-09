d603c89b539150408e1776db97d71666e7baab5c
EJ <re.johnson547@gmail.com>
2020-06-08 21:47:23 -0400

    update

--- email_project.py
+++ email_project.py
@@ -1,5 +1,29 @@
 # 06/07/2020
 ''' This project is going to send me the weather via email or text.'''
 
-import ezgmail
+import smtplib
 import os 
+import ssl
+
+port = 465
+
+sender = 'ejpythonbot123@gmail.com'
+
+password = 'Sonofgina1'
+
+recieve = 'son_of_gina@yahoo.com'
+
+message = """\
+Subject: Python Email Tutorial
+
+This is from python!
+
+Ej test bot """
+
+print('Starting to send')
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
+    server.login(sender, password)
+    server.sendmail(sender, recieve, message)
+
+print('sent email')
